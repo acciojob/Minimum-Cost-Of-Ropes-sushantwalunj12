8c00cb369cfaa254ea0901e8d1e829aafb17e0b6
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-02-24 01:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-function minimumCost() {
+let formEle = document.getElementById('form');
+formEle.addEventListener('submit' , ropes);
+function ropes(event){
+	event.preventDefault();
+
+	const input = document.getElementById('form').elements[0].value;
+
+   const arr = input.split(",");
+   console.log(arr);
+
+   for(let i=0; i<arr.length; i++){
+	 arr[i] = parseInt(arr[i]);
+   }
+
+
+   let arrRope = [];
+
+	while(arr.length != 1){ 
+    let add = 0;
+	for(let i=1; i<=2; i++){
+	//find 1st min value
+	let min1 = arr[0];
+	let minIndex = 0;
+	for(let index=1; index<arr.length; index++){
+		if(arr[index] < min1){ 
+			min1 = arr[index];
+			minIndex = index;
+		}
+	}
     
-    var res = 0;
-    var cost = 0;
-    var inputArray = document.getElementById("inputArr").value;
-    inputArray = inputArray.split(',').map(Number);
-    for (let i = 0; i < inputArray.length - 1; i += 0) {
-        if (inputArray.length == 0) break;
-        inputArray = inputArray.sort((a,b) => b-a);
-        res = inputArray.pop() + inputArray.pop();
-        cost += res;
-        inputArray[inputArray.length] = res;   
-    }  
-    document.getElementById("result").innerHTML = cost;
+	add = add + min1;
+    arr.splice(minIndex,1);
+	}
+
+	arr.push(add);
+
+	arrRope.push(add);
+	}
+	let cost = 0;
+	arrRope.forEach((value) => {
+		cost = cost + value;
+	})
+	console.log(cost);
+
+	document.getElementById('result').innerHTML = cost;
+	console.log("hello");
 }
