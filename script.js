ae840e6d7a9f841a693e385f35e0d0a196e2371a
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-02-23 19:35:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-//your code here
-const form = document.querySelector('#input-form');
-const resultDiv = document.querySelector('#result');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  
-
-  const inputValues = event.target.elements.ropes.value
-    .split(',')
-    .map(val => parseInt(val.trim()));
-
-
-  inputValues.sort((a, b) => a - b);
-
-
-  let totalCost = 0;
-  let lastIndex = inputValues.length - 1;
-
-  while (lastIndex > 0) {
-    const cost = inputValues[lastIndex] + inputValues[lastIndex - 1];
-    totalCost += cost;
-    inputValues.splice(lastIndex - 1, 2, cost);
-
-    lastIndex--;
-  }
-
-  resultDiv.textContent = `Minimum cost of ropes: ${totalCost}`;
-});
-
+function minimumCost() {
+    
+    var res = 0;
+    var cost = 0;
+    var inputArray = document.getElementById("inputArr").value;
+    inputArray = inputArray.split(',').map(Number);
+    for (let i = 0; i < inputArray.length - 1; i += 0) {
+        if (inputArray.length == 0) break;
+        inputArray = inputArray.sort((a,b) => b-a);
+        res = inputArray.pop() + inputArray.pop();
+        cost += res;
+        inputArray[inputArray.length] = res;   
+    }  
+    document.getElementById("result").innerHTML = cost;
+}
